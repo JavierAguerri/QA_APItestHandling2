c59f032e67ca997202518bb11ff7f11192e0c04e
JavierAguerri <javieraguerrimoreno@gmail.com>
2023-01-13 19:07:20 +0100

    add a getSearch method to All which returns the search object to any of its fields

--- all.py
+++ all.py
@@ -1,4 +1,7 @@
 from dataclasses import dataclass
+from searchGeneral import SearchGeneral
+
+import requests, json
 
 @dataclass
 class All:
@@ -12,3 +15,9 @@ class All:
     @classmethod
     def from_json(cls, json_data: dict):
         return cls(**json_data)
+    
+    def getSearch(self, field, endpointClass):
+        url = getattr(self, field)
+        response = requests.get(url)
+        json_response = json.loads(response.text)
+        return SearchGeneral.from_json(json_response, endpointClass)
